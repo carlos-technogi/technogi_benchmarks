aea438c2bc04599d47645ef06cb0ff15e0511c0c
Carlos Hernandez <carlos@technogi.com.mx>
2014-08-08 14:50:32 -0500

    added jruby rails benchmark

---
+++ jrails_bm/app/controllers/benchmark_controller.rb
@@ -0,0 +1,13 @@
+class BenchmarkController < ApplicationController
+  def p1
+  end
+
+  def p2
+  end
+
+  def p3
+  end
+
+  def p4
+  end
+end
---
+++ play_scala_bm/build.sbt
@@ -0,0 +1,7 @@
+import play.Project._
+
+name := "play_scala_bm"
+
+version := "1.0"
+
+playScalaSettings
